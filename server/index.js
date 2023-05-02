54680e7cbc5b095bcf5df87c1d9a7e8ea92db8c0
Alex-Shatskiy <alexlev706@gmail.com>
2023-05-03 11:29:52 +1200

    test

--- server/index.js
+++ server/index.js
@@ -7,7 +7,10 @@ client.connect()
 const app = express()
 const port = 5000
 
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  credentials: true
+}));
 
 app.get('/', async (req,res) =>{
   

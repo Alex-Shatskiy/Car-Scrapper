d32f1ab7c1e9d78797dc775e9b761dd3d1c09bf5
Alex-Shatskiy <alexlev706@gmail.com>
2023-05-03 11:23:18 +1200

    test

--- server/index.js
+++ server/index.js
@@ -9,14 +9,6 @@ const port = 5000
 
 app.use(cors())
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://car-scrapper-alex.onrender.com"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-
-
 app.get('/', async (req,res) =>{
   
 })

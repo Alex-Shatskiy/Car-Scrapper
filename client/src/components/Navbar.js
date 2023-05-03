9ab48a80c82ff2de8aa737b47c6c3bab231be73f
Alex-Shatskiy <alexlev706@gmail.com>
2023-05-04 00:05:48 +1200

    mobile optermization

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,9 +7,14 @@ const Navbar = (props) => {
   const { setData, placeHolder, setPlaceHolder, filter, setFilter, dbType, setDbType } = props
 
   const [carCompany, setCarCompany] = useState("")
+  const [isOpen, setIsOpen] = useState(false)
 
   const getCars = async (company) => {
-   await axios.get(`https://car-scrapper.onrender.com/${company}`)
+   await axios({
+    method: 'get',
+    url: `http://localhost:5000/${company}`,
+    withCredentials: false,
+  })
    .then((res) => setData(res.data))
    .catch(err =>console.log("WHoops:",err))
   }
@@ -31,16 +36,16 @@ const Navbar = (props) => {
     getCars(url)
     setDbType(dbType)
   }
+
   return (
     <>
       <div className="nav-bar">
         <img className="reddit-icon" src="./images/web-crawlers.jpg" />
         <div className="nav-bar-navigation">
-          <h3>
-            Car Scraper for{" "}
-            {placeHolder == "Select Company" ? "LOL" : placeHolder}
-          </h3>
-          <div className="subreddit-selection">
+          <div className="website-name">
+          <h3>Car Finder</h3>
+          </div>
+          <div className="subreddit-selection ">
             <span>{placeHolder}</span>
             <div className="section-contnet">
               <p
